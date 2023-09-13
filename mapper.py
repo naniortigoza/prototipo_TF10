a1688946b33cc3943450f322d399b24646882825
Danissa Ortigoza <danissa.oa@gmail.com>
2023-09-13 08:59:23 -0400

    13/09/2023-08:59

--- mapper.py
+++ mapper.py
@@ -6,12 +6,15 @@ import joblib  # Importa la biblioteca necesaria para cargar el modelo
 # Cargar el modelo entrenado
 modelo = joblib.load('modelo_random_forest.pkl')
 
+# Leer la primera línea que contiene los encabezados y omitirla
+header = next(sys.stdin)
+
 for linea in sys.stdin:
     # Supongamos que cada línea contiene datos de uno de los archivos
     datos = linea.strip().split(',')  # Suponiendo que los datos están separados por comas
     
-    # Verificar el tipo de datos
-    if len(datos) == 6:
+    # Verificar el tipo de datos y omitir encabezados en cada archivo
+    if len(datos) == 6 and datos[0] != 'ID_Datos':
         # Este es un registro de datos_economicos.csv
         ID_Datos, ID_Zona, Fecha, Tasa_Inflacion, Variacion_PIB, Precio_m2 = datos
         
@@ -21,7 +24,7 @@ for linea in sys.stdin:
         # Emitir resultados de predicción (clave, valor)
         print(f'Prediccion_Datos_Economicos\t{ID_Datos}\t{modelo.predict(datos)}')
     
-    elif len(datos) == 10:
+    elif len(datos) == 10 and datos[0] != 'Propiedad_ID':
         # Este es un registro de datos_propiedades.csv
         Propiedad_ID, Zona_ID, Tipo_Propiedad, Ubicacion, Tamano, Habitaciones, Precio, Antiguedad, Caracteristicas, Ubicacion_Especial = datos
         
