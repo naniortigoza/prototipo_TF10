5ef7062cc5291f04e0d82a265b7691b9e59fb231
Danissa Ortigoza <danissa.oa@gmail.com>
2023-09-13 14:08:43 -0400

    22

--- reducer.py
+++ reducer.py
@@ -12,8 +12,14 @@ csv_writer.writerow(['Propiedad_ID', 'Precio_Predicho'])
 # Procesa las líneas de entrada
 for linea in sys.stdin:
     # Parsea la entrada del mapper
-    clave, Propiedad_ID, valor_prediccion = linea.strip().split('\t')
-    
-    # Escribe los resultados al archivo CSV
-    if clave == 'Prediccion':
-        csv_writer.writerow([Propiedad_ID, valor_prediccion])
+    partes = linea.strip().split('\t')
+
+    # Verifica si hay suficientes partes
+    if len(partes) == 3:
+        clave, Propiedad_ID, valor_prediccion = partes
+
+        # Escribe los resultados al archivo CSV
+        if clave == 'Prediccion':
+            csv_writer.writerow([Propiedad_ID, valor_prediccion])
+    else:
+        print(f"Error en la línea: {linea.strip()}")
