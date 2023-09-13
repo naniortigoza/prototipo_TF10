d6798ea6e89fbaca82ae004740289f6b26131ec1
Danissa Ortigoza <danissa.oa@gmail.com>
2023-09-13 12:24:41 -0400

    13/09/2023-12:24

--- reducer.py
+++ reducer.py
@@ -3,22 +3,22 @@
 import sys
 
 # Inicializa variables para realizar cálculos de resumen
-total_predicciones_economicas = 0
-suma_predicciones_economicas = 0.0
+total_predicciones = 0
+suma_predicciones = 0.0
 
 for linea in sys.stdin:
     # Parsea la entrada del mapper
-    clave, ID, valor_prediccion = linea.strip().split('\t')
+    clave, Propiedad_ID, valor_prediccion = linea.strip().split('\t')
     
     # Realiza cálculos de resumen con los valores
     valor_prediccion = float(valor_prediccion)
     
-    if clave == 'Prediccion_Datos_Economicos':
-        total_predicciones_economicas += 1
-        suma_predicciones_economicas += valor_prediccion
+    if clave == 'Prediccion':
+        total_predicciones += 1
+        suma_predicciones += valor_prediccion
 
 # Calcula estadísticas finales
-promedio_predicciones_economicas = suma_predicciones_economicas / total_predicciones_economicas
+promedio_predicciones = suma_predicciones / total_predicciones if total_predicciones > 0 else 0
 
 # Emite el resultado final
-print(f'Promedio_Predicciones_Datos_Economicos\t{promedio_predicciones_economicas}')
+print(f'Promedio_Predicciones_Propiedades\t{promedio_predicciones}')
