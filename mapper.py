f955dbda58c51ce5ccb548483a6f8d6e90b44493
Danissa Ortigoza <danissa.oa@gmail.com>
2023-09-13 12:02:18 -0400

    13/09/2023-12:02

--- mapper.py
+++ mapper.py
@@ -9,30 +9,14 @@ modelo = joblib.load('modelo_random_forest.pkl')
 # Leer la primera línea que contiene los encabezados y omitirla
 header = next(sys.stdin)
 
-# Crear un DataFrame para almacenar los datos combinados
-datos_combinados = []
-
 for linea in sys.stdin:
     # Supongamos que cada línea contiene datos de uno de los archivos
     datos = linea.strip().split(',')  # Suponiendo que los datos están separados por comas
     
-    # Verificar el tipo de datos y omitir encabezados en cada archivo
-    if len(datos) == 8 and datos[0] != 'ID_Datos':
-        # Este es un registro de datos_economicos.csv
-        ID_Datos, ID_Zona, Dia, Mes, Anho, Tasa_Inflacion, Variacion_PIB, Precio_m2 = datos
-        
-        # Convertir características a números flotantes
-        datos_para_modelo = [
-            float(Tasa_Inflacion),
-            float(Variacion_PIB),
-            float(Precio_m2),
-        ]
-        
-        datos_combinados.append(datos_para_modelo)
-    
-    elif len(datos) == 10 and datos[0] != 'Propiedad_ID':
+    # Verificar si la línea tiene el número correcto de campos
+    if len(datos) == 6 and datos[0] != 'Propiedad_ID':
         # Este es un registro de datos_propiedades.csv
-        Propiedad_ID, Zona_ID, Tipo_Propiedad, Ubicacion, Tamanho, Habitaciones, Precio, Antiguedad, Carac_Adicionales, Ubicacion_Especial = datos
+        Propiedad_ID, Tasa_Inflacion, Variacion_PIB, Precio_m2, Tamanho, Habitaciones = datos
         
         # Convertir características a números flotantes
         datos_para_modelo = [
@@ -43,11 +27,8 @@ for linea in sys.stdin:
             float(Habitaciones),
         ]
         
-        datos_combinados.append(datos_para_modelo)
+        # Realizar la predicción para esta línea
+        resultado_prediccion = modelo.predict([datos_para_modelo])
 
-# Realizar predicciones en el conjunto de datos combinados
-resultados_prediccion = modelo.predict(datos_combinados)
-
-# Emitir resultados de predicción (clave, valor)
-for resultado in resultados_prediccion:
-    print(f'Prediccion\t{resultado}')
+        # Emitir resultados de predicción (clave, valor)
+        print(f'Prediccion\t{Propiedad_ID}\t{resultado_prediccion[0]}')
