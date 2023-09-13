10a4e3626c00d5ca19dd1d3aa9a198052cd6f781
Danissa Ortigoza <danissa.oa@gmail.com>
2023-09-13 11:21:57 -0400

    13/09/2023-11:21

--- mapper.py
+++ mapper.py
@@ -2,13 +2,16 @@
 
 import sys
 import joblib  # Importa la biblioteca necesaria para cargar el modelo
-from datetime import datetime  # Importa la biblioteca para trabajar con fechas
+import pandas as pd  # Importa Pandas para manipular los datos
 
 # Cargar el modelo entrenado
 modelo = joblib.load('modelo_random_forest.pkl')
 
 # Leer la primera línea que contiene los encabezados y omitirla
 header = next(sys.stdin)
+
+# Crear un DataFrame para almacenar los datos combinados
+datos_combinados = []
 
 for linea in sys.stdin:
     # Supongamos que cada línea contiene datos de uno de los archivos
@@ -19,36 +22,34 @@ for linea in sys.stdin:
         # Este es un registro de datos_economicos.csv
         ID_Datos, ID_Zona, Dia, Mes, Anho, Tasa_Inflacion, Variacion_PIB, Precio_m2 = datos
         
-        # Convertir características a números flotantes y pasar al modelo
+        # Convertir características a números flotantes
         datos_para_modelo = [
-            [
-                float(Tasa_Inflacion),
-                float(Variacion_PIB),
-                float(Precio_m2),
-                float(Tamanho),
-                float(Habitaciones)
-            ]
-        ]  # Convierte a matriz 2D
-        resultado_prediccion = modelo.predict(datos_para_modelo)
+            float(Tasa_Inflacion),
+            float(Variacion_PIB),
+            float(Precio_m2),
+        ]
         
-        # Emitir resultados de predicción (clave, valor)
-        print(f'Prediccion_Datos_Economicos\t{ID_Datos}\t{resultado_prediccion}')
+        datos_combinados.append(datos_para_modelo)
     
     elif len(datos) == 10 and datos[0] != 'Propiedad_ID':
         # Este es un registro de datos_propiedades.csv
         Propiedad_ID, Zona_ID, Tipo_Propiedad, Ubicacion, Tamanho, Habitaciones, Precio, Antiguedad, Carac_Adicionales, Ubicacion_Especial = datos
         
-        # Convertir características a números flotantes y pasar al modelo
+        # Convertir características a números flotantes
         datos_para_modelo = [
-            [
-                float(Tasa_Inflacion),
-                float(Variacion_PIB),
-                float(Precio_m2),
-                float(Tamanho),
-                float(Habitaciones)
-            ]
-        ]  # Convierte a matriz 2D
-        resultado_prediccion = modelo.predict(datos_para_modelo)
+            float(Tamanho),
+            float(Habitaciones),
+            float(Precio),
+        ]
         
-        # Emite resultados de predicción para datos de propiedades
-        print(f'Prediccion_Datos_Propiedades\t{Propiedad_ID}\t{resultado_prediccion}')
+        datos_combinados.append(datos_para_modelo)
+
+# Convertir la lista de datos combinados en un DataFrame
+datos_para_modelo_df = pd.DataFrame(datos_combinados)
+
+# Realizar predicciones en el conjunto de datos combinados
+resultados_prediccion = modelo.predict(datos_para_modelo_df)
+
+# Emitir resultados de predicción (clave, valor)
+for resultado in resultados_prediccion:
+    print(f'Prediccion\t{resultado}')
