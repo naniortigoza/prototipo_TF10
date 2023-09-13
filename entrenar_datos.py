b59e6b75591bf828d88337f9ca3c3a75b4f9a3e7
Danissa Ortigoza <danissa.oa@gmail.com>
2023-09-13 11:29:38 -0400

    13/09/2023-11:29

--- entrenar_datos.py
+++ entrenar_datos.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import joblib
-from sklearn.model_selection import train_test_split        # pip install scikit-learn
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 
@@ -12,35 +11,23 @@ datos_propiedades = pd.read_csv('datos/datos_propiedades.csv')
 datos_combinados = datos_propiedades.merge(datos_economicos, left_on='Zona_ID', right_on='Zona_ID', how='inner')
 
 # Preparar los datos
-X = datos_combinados[['Tasa_Inflacion', 'Variacion_PIB', 'Tamanho', 'Habitaciones', 'Tipo_Propiedad']]
+X = datos_combinados[['Tasa_Inflacion', 'Variacion_PIB', 'Precio_m2', 'Tamanho', 'Habitaciones']]
 y = datos_combinados['Precio']
-
-# Dividir los datos en conjunto de entrenamiento y prueba
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Ajustar el modelo Random Forest
 rf_model = RandomForestRegressor(n_estimators=200, random_state=42, max_depth=10)  # Añadir max_depth para evitar sobreajuste
-rf_model.fit(X_train, y_train)
+rf_model.fit(X, y)
 
-# Realizar predicciones en el conjunto de prueba
-y_pred = rf_model.predict(X_test)
+# Realizar predicciones en el conjunto de datos
+y_pred = rf_model.predict(X)
 
 # Calcular el error cuadrático medio (MSE)
-mse = mean_squared_error(y_test, y_pred)
+mse = mean_squared_error(y, y_pred)
 print(f'Error cuadrático medio (MSE): {mse}')
 
 # Calcular el coeficiente de determinación R^2
-r2 = r2_score(y_test, y_pred)
+r2 = r2_score(y, y_pred)
 print(f'Coeficiente de determinación (R^2): {r2}')
-
-# Guardar el modelo entrenado en un archivo
-modelo_archivo = 'modelo_random_forest.pkl'
-joblib.dump(rf_model, modelo_archivo)
-print(f'Modelo guardado en {modelo_archivo}')
-
-# Exportar los resultados de predicción a un archivo CSV
-resultados = pd.DataFrame({'Precio_Real': y_test, 'Precio_Predicho': y_pred})
-resultados.to_csv('datos/resultados_prediccion.csv', index=False)
 
 # Obtener la importancia de las variables
 importancias = rf_model.feature_importances_
@@ -48,4 +35,11 @@ print('\nImportancia de las variables:')
 for i, importancia in enumerate(importancias):
     print(f'Variable {X.columns[i]}: {importancia}')
 
+# Exportar los resultados de predicción a un archivo CSV con el precio real y predicho
+resultados = pd.DataFrame({'Precio_Real': y, 'Precio_Predicho': y_pred})
+resultados.to_csv('datos/resultados_prediccion.csv', index=False)
 
+# Guardar el modelo entrenado en un archivo
+modelo_archivo = 'modelo_random_forest.pkl'
+joblib.dump(rf_model, modelo_archivo)
+print(f'Modelo guardado en {modelo_archivo}')
